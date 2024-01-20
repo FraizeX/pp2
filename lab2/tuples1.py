dd9872a3ac6f0d5ceb7429fec9a647bc0c2ba709
Danial Kairov <fraizer_go@mail.ru>
2024-01-20 13:07:36 +0600

    tuples1 example

---
+++ lab2/tuples1.py
@@ -0,0 +1,3 @@
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple) #cout ('apple', 'banana', 'cherry', 'apple', 'cherry')
+#tuples is ordered, unchangeable, allow duplicates
